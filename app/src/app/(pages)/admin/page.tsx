d3d94a774e917a22ab238cf26e85900bcb54f0ca
Teddy Blanco <57070070+tonTed@users.noreply.github.com>
2024-07-19 11:56:26 -0400

    Update page.tsx add fragment

--- app/src/app/(pages)/admin/page.tsx
+++ app/src/app/(pages)/admin/page.tsx
@@ -27,23 +27,25 @@ function SchoolCreate() {
   const { setOpen } = CreateSchoolModalContext;
 
   return (
-    <div className={vstack({
-      gap: "20",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100vh"
-  })}>
-      <h1 className={css({
-          fontSize: "4xl",
-          fontWeight: "bold",
-          color: "text"
-      })}>{'Ce compte n'a pas d'école associée'}</h1>
-      <Button onClick={() => setOpen(true)} size={"2xl"}>
-          Créer une école
-          <School/>
-      </Button>
-      <CreateSchoolModal />
-    </div>
+    <>
+      <div className={vstack({
+        gap: "20",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh"
+      })}>
+        <h1 className={css({
+            fontSize: "4xl",
+            fontWeight: "bold",
+            color: "text"
+        })}>{'Ce compte n'a pas d'école associée'}</h1>
+        <Button onClick={() => setOpen(true)} size={"2xl"}>
+            Créer une école
+            <School/>
+        </Button>
+        <CreateSchoolModal />
+      </div>
+    </>
   );
 }
 

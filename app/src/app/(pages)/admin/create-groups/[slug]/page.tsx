da72d12c402f74e1c89e80d42551b4f9aa1cedb9
Teddy <yddetco@gmail.com>
2024-07-22 16:23:36 -0400

    Improve prompting

--- app/src/app/(pages)/admin/create-groups/[slug]/page.tsx
+++ app/src/app/(pages)/admin/create-groups/[slug]/page.tsx
@@ -71,6 +71,26 @@ export default function CreateGroups() {
   }, []);
 
   const handleCreateGroup = async () => {
+
+    const studentAmount = students.length;
+    const studentGroupAmount = Math.floor(studentAmount / 3);
+    const remainingStudents = studentAmount % 3;
+
+    const directive = () => {
+      switch (remainingStudents) {
+        case 0:
+          return `Tu dois créer ${studentGroupAmount} groupes de 3 élèves.`;
+        case 1:
+          return `Tu dois créer ${studentGroupAmount - 1} groupes de 3 élèves et un groupe de 4 eleves.`;
+        case 2:
+          return `Tu dois créer ${studentGroupAmount - 1} groupes de 3 élèves et un groupe de 2 élèves.`;
+        default:
+          return `Tu dois créer ${studentGroupAmount} groupes de 3 élèves.`;
+      }
+    };
+
+    console.log(directive());
+
     const studentsPrompt = students.map((student) => {
       return `{"id": "${student.publicKey.toString()}", "availability": "${student.account.availability}", "interests": "${student.account.interests}", "skills": "${student.account.skills.join(", ")}"}`;
     });
@@ -82,7 +102,7 @@ export default function CreateGroups() {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          "prompt": JSON.stringify(studentsPrompt),
+          "prompt": `${directive()}\n${JSON.stringify(studentsPrompt)}`,
         })
       }
     );
